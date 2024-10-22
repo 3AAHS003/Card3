7d819d7e0e8dc515ceda2639fe9dddbea3620c97
3AAHS003 <00001066436034sp@al.educacao.sp.gov.br>
2024-10-22 17:28:47 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Biologia',
+    'Qual a funçaõ do cerebro?',
+    'Nos deixar com pensamentos desesperados sobre o futuro!'
+)
+
+
+criaCartao(
+    'Programação',
+    'O que é o Marcio vai me dar?',
+    'Um 10 na médiaaaa!!!!'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'You speak English?',
+    'I dont speak com vacilão'
+)
+criaCartao(
+    'Extra',
+    'UwU',
+    'Ficou Maneiro fala ai'
+)
